b28d66ac6df4ecd886a250c49d8923996593f8db
Chris Samuelson <chris.sam55@gmail.com>
2014-07-18 02:19:06 -0400

    Initial commit

---
+++ src/communityhub/CommunityHub.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package communityhub;
+
+/**
+ *
+ * @author Chris
+ */
+public class CommunityHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
