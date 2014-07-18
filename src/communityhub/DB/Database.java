c8f1fbd9ec6376a10653eaf6cc9219ec933a4ace
Chris Samuelson <chris.sam55@gmail.com>
2014-07-18 14:59:32 -0400

    Added interface to retrieve low and mid perm users from DB

--- src/communityhub/DB/Database.java
+++ src/communityhub/DB/Database.java
@@ -16,10 +16,11 @@ public class Database{
     return null;
   }
   // given an announcement object, it creates it in the db
+
   public static UUID createAnnouncement(Announcement ann){
     return null;
   }
-  
+
   public static boolean editAnnouncement(UUID oldID, Announcement newAnn){
     return false;
   }
@@ -27,36 +28,42 @@ public class Database{
   public static ArrayList<Announcement> getRecentAnnouncements(){
     return null;
   }
-  
+
   public static Announcement getAnnouncement(String annID){
     return null;
   }
-  
+
   public static boolean deleteAnnouncement(String annID){
     return false;
   }
-  
+
   public static UUID CreatePost(Post p){
     return null;
   }
-  
+
   public static boolean editPost(UUID oldID, Post p){
     return false;
   }
-  
+
   public static ArrayList<Post> getRecentPosts(){
     return null;
   }
-  
+
   public static Post getPost(String postID){
     return null;
   }
-  
+
   public static boolean deletePost(String postID){
     return false;
   }
-  
+
   public static boolean changeRole(String username, BasicUser New){
     return false;
+  }
+
+  public static ArrayList<BasicUser> getPotentialUsers(){
+    //Find list of low and mid perm users
+    //Generally to be used for admins to upgrade users to or from mods
+    return null;
   }
 }
