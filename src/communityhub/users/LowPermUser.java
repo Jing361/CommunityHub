70cb413c9a09d96e7fbde963f6c3d71014ac98bf
Chris Samuelson <chris.sam55@gmail.com>
2014-07-25 01:12:13 -0400

    proper userstring for LowPermUser

--- src/communityhub/users/LowPermUser.java
+++ src/communityhub/users/LowPermUser.java
@@ -12,4 +12,10 @@ public class LowPermUser extends BasicUser{
   public LowPermUserGUI SpawnGui(){
     return new LowPermUserGUI(this);
   }
+
+  @Override
+  public String toString(){
+    return "user";
+  }
+
 }
